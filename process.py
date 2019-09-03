a2087fbfdbf3851c53951715be0dd3e33edf4011
ErikBoesen <me@erikboesen.com>
2019-09-03 19:42:24 -0400

    Add starting script

---
+++ process.py
@@ -0,0 +1,31 @@
+
+with open("page.html", "r", encoding="iso-8859-1") as f:
+    print("Parsing into BeautifulSoup")
+    page = BeautifulSoup(f.read(), "html.parser")
+print("Finding .student_container elements")
+containers = page.find_all("div", {"class": "student_container"})
+students = []
+for container in containers:
+    info = container.find_all("div", {"class": "student_info"})
+    college = info[0].text
+    email = info[1].find("a").text
+    trivia = info[1].find_all(text=True, recursive=False)
+    room = trivia.pop(0)
+    birthday = trivia.pop()
+    major = trivia.pop()
+    address = "\n".join(trivia)
+    students.append({
+        "image_id": container.find("div", {"class": "student_img"}).find("img")["src"][len("/facebook/Photo?id="):],
+        "year": int(container.find("div", {"class": "student_year"}).text.replace("'", "20")),
+        "name": container.find("h5", {"class": "yalehead"}).text,
+        "pronoun": container.find("div", {"class": "student_info_pronoun"}).text,
+        "room": room,
+        "birthday": birthday,
+        "major": major,
+        "address": address,
+    })
+    print(students)
+    break
+
+with open("students.csv", "w") as f:
+    writer = csv.writer(f)
