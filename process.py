aae85aedf7ec020d5cfa10f8db2da126dafc78fe
ErikBoesen <me@erikboesen.com>
2019-09-03 19:43:43 -0400

    Split name

--- process.py
+++ process.py
@@ -7,6 +7,8 @@ containers = page.find_all("div", {"class": "student_container"})
 students = []
 for container in containers:
     info = container.find_all("div", {"class": "student_info"})
+    name = container.find("h5", {"class": "yalehead"}).text,
+    surname, forename = name.split(", ", 1)
     college = info[0].text
     email = info[1].find("a").text
     trivia = info[1].find_all(text=True, recursive=False)
@@ -15,9 +17,10 @@ for container in containers:
     major = trivia.pop()
     address = "\n".join(trivia)
     students.append({
+        "forename": forename,
+        "surname": surname,
         "image_id": container.find("div", {"class": "student_img"}).find("img")["src"][len("/facebook/Photo?id="):],
         "year": int(container.find("div", {"class": "student_year"}).text.replace("'", "20")),
-        "name": container.find("h5", {"class": "yalehead"}).text,
         "pronoun": container.find("div", {"class": "student_info_pronoun"}).text,
         "room": room,
         "birthday": birthday,
