f6bdb642bf2cf403318db60569366c4ce48c476d
ErikBoesen <me@erikboesen.com>
2019-09-03 20:13:00 -0400

    Make sure first-years have emails listed

--- analyze.py
+++ analyze.py
@@ -4,4 +4,4 @@ with open("students.csv") as f:
     students = [{k: int(v) for k, v in row.items()}
                 for row in csv.DictReader(f, skipinitialspace=True)]
 
-print(",".join([student for student in students if year == 2023]))
+print(",".join([student for student in students if student["year"] == 2023 and student["email"]]))
