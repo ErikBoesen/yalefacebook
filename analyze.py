5384ef2950eceb54f81bef6164340f581db95bd3
ErikBoesen <me@erikboesen.com>
2019-09-03 20:21:41 -0400

    Simplify analysis code

--- analyze.py
+++ analyze.py
@@ -3,4 +3,6 @@ import csv
 with open("students.csv") as f:
     students = [{k: v for k, v in row.items()}
                 for row in csv.DictReader(f, skipinitialspace=True)]
-print(",".join([student for student in students if int(student["year"]) == 2023 and student["email"]]))
+
+emails = [student["email"] for student in students if int(student["year"]) == 2023 and student["email"]]
+print(",".join(emails))
