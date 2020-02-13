b0142231982d95ab46fc02222660316d948b0ef8
ErikBoesen <me@erikboesen.com>
2020-02-13 00:15:09 -0500

    Fix filename and outputting in read_brothers.py

--- read_brothers.py
+++ read_brothers.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('brothers.tsv', 'r') as f:
+with open('brothers.csv', 'r') as f:
     reader = csv.reader(f, delimiter='\t')
     for row in reader:
-        print(row[0])
+        print('\'' + row[0] + '\',')
