b2ff1bb5108ca65d19c14942f16162f53157f187
ErikBoesen <me@erikboesen.com>
2020-02-13 00:11:27 -0500

    Read cookie from file rather than hardcoding

--- get.py
+++ get.py
@@ -7,8 +7,11 @@ import time
 
 DEBUG = False
 
+with open('cookie.txt', 'r') as f:
+    cookie = f.read().strip()
+
 headers = {
-    'Cookie': 'JSESSIONID=40F7C9727225DB92A2BA243D11921A44; __cfduid=d3e64860ff3a21bc0bce6269c2614af8a1565532230; _ga=GA1.2.2025653848.1578368543',
+    'Cookie': cookie,
 }
 
 filename = 'page.html'
